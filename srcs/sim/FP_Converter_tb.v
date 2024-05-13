ae20e9053e1aa368c53f5f227ce061a726c32ea3
quoc-thinh <lquocthinh10a1@gmail.com>
2024-05-13 23:08:38 +0700

    convert

--- srcs/sim/FP_Converter_tb.v
+++ srcs/sim/FP_Converter_tb.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
-`include "../sources/Convert_Floatingpoint.v"
+`include "../sources/Converter_FloatingPoint/FP_to_fixed.v"
 
 module Converter_tb #(parameter XLEN = 32);
 reg [XLEN-1:0] A;
 
-wire [8:0] result;
+wire [31:0] result;
 real  value;
 
-Converter F_Add (.A(A),.result(result));
+FP_sang_TP F_Add (.a(A),.result(result));
 
 initial  
 begin
@@ -16,7 +16,7 @@ A = 32'b0_10000000_10011001100110011001100;  // 3.2
 #20
 A = 32'b11000010100011000011111011111010;  // 70.123
 #20
-A = 32'b1_01111110_00000000000000000000000;  // -0.5
+A = 32'b10111101111111000010100010000000;  // -0.5
 #20
 A = 32'b01000001011100011001100110011010;  // 15.1
 #20
@@ -25,15 +25,15 @@ end
 
 initial
 begin
-#20
-$display("Result : %f",result);
-#20
-$display("Result : %f",result);
-#20
-$display("Result : %f",result);
-#20
-$display("Result : %f",result);
-#20
-$finish;
+// #20
+// $display("Result : %f",result);
+// #20
+// $display("Result : %f",result);
+// #20
+// $display("Result : %f",result);
+// #20
+// $display("Result : %f",result);
+// #20
+// $finish;
 end
 endmodule
